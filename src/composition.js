3d6f1064a53cae31c2ec6722f5946db60198830a
Isaac450 <isaacmendes96@gmail.com>
2026-04-18 17:07:59 +0100

    wip

--- src/composition.js
+++ src/composition.js
@@ -1,23 +1,16 @@
 // src/composition.js
 
 // Adaptadores de infraestrutura
-const RedisEventBus              =
-require('./infrastructure/RedisEventBus');
-const SqlAppointmentRepository   =
-require('./infrastructure/SqlAppointmentRepository');
-const SqlPatientRepository       =
-require('./infrastructure/SqlPatientRepository');
-const TwilioNotificationService  =
-require('./infrastructure/TwilioNotificationService');
-const SqlBillingRepository       =
-require('./infrastructure/SqlBillingRepository');
-const SqlAuditRepository         =
-require('./infrastructure/SqlAuditRepository');
+const RedisEventBus              = require('./infrastructure/RedisEventBus');
+const SqlAppointmentRepository   = require('./infrastructure/SqlAppointmentRepository');
+const SqlPatientRepository       = require('./infrastructure/SqlPatientRepository');
+const TwilioNotificationService  = require('./infrastructure/TwilioNotificationService');
+const SqlBillingRepository       = require('./infrastructure/SqlBillingRepository');
+const SqlAuditRepository         = require('./infrastructure/SqlAuditRepository');
 
 // Core
 const AppointmentService   = require('./core/services/AppointmentService');
-const NotificationConsumer =
-require('./core/consumers/NotificationConsumer');
+const NotificationConsumer = require('./core/consumers/NotificationConsumer');
 const BillingConsumer      = require('./core/consumers/BillingConsumer');
 const AuditConsumer        = require('./core/consumers/AuditConsumer');
 
@@ -41,8 +34,7 @@ const auditConsumer        = new AuditConsumer(auditRepository);
 
 // 4. Registar os Consumers no Event Bus
 //    O Bus chama consumer.handle(event) quando o evento chegar
-eventBus.subscribe('ConsultaAgendada', (e) =>
-notificationConsumer.handle(e));
+eventBus.subscribe('ConsultaAgendada', (e) => notificationConsumer.handle(e));
 eventBus.subscribe('ConsultaAgendada', (e) => billingConsumer.handle(e));
 eventBus.subscribe('ConsultaAgendada', (e) => auditConsumer.handle(e));
 
